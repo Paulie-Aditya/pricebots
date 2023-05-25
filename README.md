0882a43f2ae4d79490498f8cf54f261b64ccc6e4
code913 <mdaatifalam2018@gmail.com>
2023-05-25 15:56:36 +0530

    feat: add interactions endpoint + change cron job to 10 minutes

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ written in python by Paulie, being rewritten in javascript by [code913](https://
 
 you need to set the following environment variables using the `wrangler secret put VARIABLE_NAME` command:
 1. an api key from [scraperapi.com](https://scraperapi.com)
-1. 30 discord bot tokens for each of the currencies mentioned in the src/tickers.ts file. tokens should be in the format `TOKEN_CAPITALIZEDDISPLAYNAME`. the display name of the currency is the second value in the map  
+1. 30 discord bot tokens and their application public keys for each of the currencies mentioned in the src/tickers.ts file. tokens should be in the format `TOKEN_CAPITALIZEDDISPLAYNAME` and public keys in the format `PUBLIC_KEY_CAPITALIZEDDISPLAYNAME`. the display name of the currency is the second value in the map  
 for example:
 ```ts
 ["algorand", "Algo"],
@@ -17,4 +17,13 @@ TOKEN_ALGO
 TOKEN_APE
 TOKEN_ATOM
 TOKEN_AVAX
+
+PUBLIC_KEY_ALGO
+PUBLIC_KEY_APE
+PUBLIC_KEY_ATOM
+PUBLIC_KEY_AVAX
 ```
+
+after setting the variables run `wrangler deploy` and copy the returned deployment url (it should look like `pricebots.something.workers.dev`). now go to discord dev portal, and for each bot go to the interactions endpoint url input and set as `<the url>/interactions?bot=<capitalized display name>`  
+for example: to set bitcoin bot's url
+![screenshot of dev portal](./interactions.png)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { verifyKey, InteractionResponseType } from "discord-interactions";
 import { Request, Router } from "itty-router";
 import { getData, setNick } from "./utils.js";
 import { CFRequest } from "@cloudflare/workers-types";
@@ -28,21 +29,34 @@ router.get("/cron", async (_, env) => {
 
 	return new Response("set nicks");
 })
+router.post("/interactions", _ => {
+	return new Response(JSON.stringify({
+      type: InteractionResponseType.PONG
+    }), {
+		headers: {
+			"content-type": "application/json;charset=UTF-8",
+		},
+	});
+});
 
 export default {
   async fetch(request: CFRequest, env: any, event: any) {
-    // if (request.method === "POST") {
-    //   const signature = request.headers.get("x-signature-ed25519");
-    //   const timestamp = request.headers.get("x-signature-timestamp");
-    //   const body = await request.clone().arrayBuffer();
-    //   const isValidRequest = signature && timestamp && verifyKey(
-    //     body,
-    //     signature,
-    //     timestamp,
-    //     env.DISCORD_PUBLIC_KEY
-    //   );
-    //   if (!isValidRequest) return new Response("Bad request signature.", { status: 401 });
-    // }
+    if (request.method === "POST") {
+		const botName = new URL(request.url)?.searchParams?.get("bot") ?? "";
+		const publicKey = env[`PUBLIC_KEY_${botName.toUpperCase()}`];
+		console.log({ botName, publicKey });
+		const signature = request.headers.get("x-signature-ed25519");
+		const timestamp = request.headers.get("x-signature-timestamp");
+		const body = await request.clone().arrayBuffer();
+		const isValidRequest = botName && signature && timestamp && verifyKey(
+			body,
+			signature,
+			timestamp,
+			publicKey ?? ""
+		);
+
+		if (!isValidRequest) return new Response("Bad request signature.", { status: 401 });
+	}
 
     return router.handle(request, env, event);
   },
